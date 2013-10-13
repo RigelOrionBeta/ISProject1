da0221a41686dfb2c789724d4ed1ecebbd514704
RigelOrionBeta <rigelorionbeta@gmail.com>
2013-10-13 17:03:32 -0400

    Create Predicate.java

---
+++ AutomaticTheoremProver/src/Predicate.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+
+public class Predicate {
+	
+	private String name;
+	private String[] arguments;
+
+	public Predicate(String name, String arguments[]) {
+		this.name = name;
+		this.arguments = arguments;
+	}
+	
+	public String toString() {
+		String retval = name + "( ";
+		for(int i = 0; i < arguments.length; i++) {
+			if(i != 0) {
+				retval += ", " + arguments[i];
+			} else {
+				retval += arguments[i];
+			}
+		}
+		retval += " )";
+		return retval;
+	}
+	
+}
